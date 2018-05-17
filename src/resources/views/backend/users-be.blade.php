ea58701602bfaf461ccabec56ed9010b2a316ada
Alexander Hofbauer <hofbauer.alexander@gmail.com>
2018-05-17 21:46:54 +0200

    [FEATURE] admin page styling
    Signed-off-by: Alexander Hofbauer <hofbauer.alexander@gmail.com>

--- src/resources/views/backend/users-be.blade.php
+++ src/resources/views/backend/users-be.blade.php
@@ -6,38 +6,39 @@
             <a href="{{ route('admin.user.backend.create') }}">{{ __('New') }}</a>
         </div>
         <div class="module__body container">
-        </div>
-    </div>
-@endsection
-
-@section('card')
-    <div class="card">
-        <div class="card__header">{{ __('Backend Users') }}</div>
-        <div class="card__body">
-            <table>
-                <thead>
-                <tr>
-                    <th>{{ __('ID') }}</th>
-                    <th>{{ __('Username') }}</th>
-                    <th>{{ __('Password') }}</th>
-                    <th></th>
-                </tr>
-                </thead>
-                <tbody>
-                @forelse ($users as $user)
-                    <tr>
-                        <td>{{ $user->id }}</td>
-                        <td>{{ $user->username }}</td>
-                        <td>{{ $user->email  }}</td>
-                        <td>
-                            <a href="{{ route('admin.users.backend.edit', ['id' => $user->id]) }}">{{ __('Edit') }}</a>
-                        </td>
-                    </tr>
-                @empty
-                    <p>No users found!</p>
-                @endforelse
-                </tbody>
-            </table>
+            <div class="card">
+                <div class="card__header">
+                    <h3>{{ __('Backend Users') }} ({{ count($users) }})</h3>
+                </div>
+                <div class="card__body">
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>{{-- Add --}}</th>
+                            <th>{{ __('ID') }}</th>
+                            <th>{{ __('Username') }}</th>
+                            <th>{{ __('Password') }}</th>
+                            <th></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @forelse ($users as $user)
+                            <tr>
+                                <td>{{-- Icon --}}</td>
+                                <td>{{ $user->id }}</td>
+                                <td>{{ $user->username }}</td>
+                                <td>{{ $user->email  }}</td>
+                                <td>
+                                    <a href="{{ route('admin.users.backend.edit', ['id' => $user->id]) }}">{{ __('Edit') }}</a>
+                                </td>
+                            </tr>
+                        @empty
+                            <p>No users found!</p>
+                        @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
