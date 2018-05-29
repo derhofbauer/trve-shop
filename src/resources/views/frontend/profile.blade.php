3b4323538c1385993db4354fcfc6fdc1fdd4f695
Alexander Hofbauer <hofbauer.alexander@gmail.com>
2018-05-29 18:27:53 +0200

    [FEATURE] finish checkout & profile logic
    Signed-off-by: Alexander Hofbauer <hofbauer.alexander@gmail.com>

--- src/resources/views/frontend/profile.blade.php
+++ src/resources/views/frontend/profile.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.frontend')
 
 @section('content')
+    <div class="nav">
+        <div class="nav-item">
+            <a href="{{ route('profile.orders') }}">{{ __('Orders') }}</a>
+        </div>
+    </div>
     @include('backend.partials.form')
 @endsection
